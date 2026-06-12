577025e9ffb6c3c30a290ad28d4edfd7f388c440
sipho-mokoena <siphowilliammokoena@gmail.com>
2026-06-12 02:16:12 +0200

    refactor(servo): remove unused servo feedback driver and test files

--- firmware/esp32-testing/include/servo_feedback_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef SERVO_FEEDBACK_TEST_H
-#define SERVO_FEEDBACK_TEST_H
-
-void setupServoFeedbackTest(int limitSwitch1Pin, int limitSwitch2Pin);
-void runServoFeedbackTest();
-int getServoLimitSwitchState(int switchIndex);
-
-#endif
--- firmware/esp32-testing/src/main.cpp
+++ firmware/esp32-testing/src/main.cpp
@@ -1,21 +1,20 @@
 #include <Arduino.h>
 
 #include "ir_obstacle_test.h"
-#include "servo_feedback_test.h"
 #include "servo_test.h"
 #include "ultrasonic_test.h"
 #include "warnings_test.h"
 
-constexpr int BUZZER_PIN = 23;
-constexpr int RED_LED_PIN = 22;
+constexpr int BUZZER_PIN = 18;
+constexpr int RED_LED_PIN = 5;
 
 constexpr int SERVO_1_PIN = 32;
 constexpr int SERVO_2_PIN = 33;
 
 constexpr int ULTRASONIC_TRIGGER_PIN = 14;
-constexpr int ULTRASONIC_ECHO_1_PIN = 26;
-constexpr int ULTRASONIC_ECHO_2_PIN = 25;
-constexpr int ULTRASONIC_ECHO_3_PIN = 27;
+constexpr int ULTRASONIC_ECHO_1_PIN = 19;
+constexpr int ULTRASONIC_ECHO_2_PIN = 22;
+constexpr int ULTRASONIC_ECHO_3_PIN = 23;
 constexpr float ULTRASONIC_OBSTACLE_DISTANCE_CM = 10.0f;
 
 constexpr int IR_SENSOR_1_PIN = 10;
@@ -25,43 +24,46 @@ constexpr int IR_SENSOR_3_PIN = 13;
 constexpr int LDR_DIGITAL_PIN = 17;
 constexpr int LDR_OUTPUT_PIN = 16;
 
-constexpr int SERVO_LIMIT_SWITCH_1_PIN = 18;
-constexpr int SERVO_LIMIT_SWITCH_2_PIN = 19;
+struct CsvData {
+    int ir1;
+    int ir2;
+    int ir3;
+    int buz;
+    int lgs;
+    int ls1;
+    int ls2;
+    int ldr;
+    int us1;
+    int us2;
+    int us3;
+    int sv1;
+    int sv2;
+};
 
-void printCsvValue(int value)
+void printStateLine(const CsvData& data)
 {
-    Serial.print(value);
-    Serial.print(", ");
-}
-
-void printCsvLine(
-    int ir1,
-    int ir2,
-    int ir3,
-    int buz,
-    int lgs,
-    int ls1,
-    int ls2,
-    int ldr,
-    int us1,
-    int us2,
-    int us3,
-    int sv1,
-    int sv2)
-{
-    printCsvValue(ir1);
-    printCsvValue(ir2);
-    printCsvValue(ir3);
-    printCsvValue(buz);
-    printCsvValue(lgs);
-    printCsvValue(ls1);
-    printCsvValue(ls2);
-    printCsvValue(ldr);
-    printCsvValue(us1);
-    printCsvValue(us2);
-    printCsvValue(us3);
-    printCsvValue(sv1);
-    Serial.println(sv2);
+    Serial.print("ir1=");
+    Serial.print(data.ir1);
+    Serial.print(", ir2=");
+    Serial.print(data.ir2);
+    Serial.print(", ir3=");
+    Serial.print(data.ir3);
+    Serial.print(", buz=");
+    Serial.print(data.buz);
+    Serial.print(", lgs=");
+    Serial.print(data.lgs);
+    Serial.print(", ldr=");
+    Serial.print(data.ldr);
+    Serial.print(", us1=");
+    Serial.print(data.us1);
+    Serial.print(", us2=");
+    Serial.print(data.us2);
+    Serial.print(", us3=");
+    Serial.print(data.us3);
+    Serial.print(", sv1=");
+    Serial.print(data.sv1);
+    Serial.print(", sv2=");
+    Serial.println(data.sv2);
 }
 
 void setup()
@@ -76,13 +78,10 @@ void setup()
         ULTRASONIC_ECHO_3_PIN);
     setupIrObstacleTest(IR_SENSOR_1_PIN, IR_SENSOR_2_PIN, IR_SENSOR_3_PIN);
     setupWarningsTest(BUZZER_PIN, RED_LED_PIN);
-    setupServoFeedbackTest(SERVO_LIMIT_SWITCH_1_PIN, SERVO_LIMIT_SWITCH_2_PIN);
 
     pinMode(LDR_DIGITAL_PIN, INPUT);
     pinMode(LDR_OUTPUT_PIN, OUTPUT);
     digitalWrite(LDR_OUTPUT_PIN, LOW);
-
-    Serial.println("IR1, IR2, IR3, BUZ, LGS, LS1, LS2, LDR, US1, US2, US3, SV1, SV2");
 }
 
 void loop()
@@ -91,15 +90,12 @@ void loop()
     runUltrasonicTest();
     runIrObstacleTest();
     runWarningsTest();
-    runServoFeedbackTest();
 
     const int ir1 = getIrObstacleState(0);
     const int ir2 = getIrObstacleState(1);
     const int ir3 = getIrObstacleState(2);
     const int buz = getBuzzerState();
     const int lgs = getLedState();
-    const int ls1 = getServoLimitSwitchState(0);
-    const int ls2 = getServoLimitSwitchState(1);
     const int ldr = digitalRead(LDR_DIGITAL_PIN) == HIGH ? 1 : 0;
     const int us1 = isUltrasonicObstacleDetected(0, ULTRASONIC_OBSTACLE_DISTANCE_CM) ? 1 : 0;
     const int us2 = isUltrasonicObstacleDetected(1, ULTRASONIC_OBSTACLE_DISTANCE_CM) ? 1 : 0;
@@ -109,7 +105,20 @@ void loop()
 
     digitalWrite(LDR_OUTPUT_PIN, ldr == 1 ? HIGH : LOW);
 
-    printCsvLine(ir1, ir2, ir3, buz, lgs, ls1, ls2, ldr, us1, us2, us3, sv1, sv2);
+    CsvData data = {
+        .ir1 = ir1,
+        .ir2 = ir2,
+        .ir3 = ir3,
+        .buz = buz,
+        .lgs = lgs,
+        .ldr = ldr,
+        .us1 = us1,
+        .us2 = us2,
+        .us3 = us3,
+        .sv1 = sv1,
+        .sv2 = sv2
+        };
+    printStateLine(data);
 
     delay(100);
 }
